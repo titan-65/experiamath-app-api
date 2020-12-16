b7075d73eb80d161792c9b75d913f2a79d2e0507
Vantol Bennett <vvantol2000@icloud.com>
2020-12-15 21:22:58 -0500

    Create Custom User
    Modified the custom user interface in which Django uses.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # experiamath-app-api
+
 ExperiMath APi source code
+
+Technologies Used: 
+
+Docker
+
+Travis CI [![Build Status](https://travis-ci.com/titan-65/experiamath-app-api.svg?branch=main)](https://travis-ci.com/titan-65/experiamath-app-api)
+
+Django
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with email. """
+        email = "test@example.com"
+        password = 'test123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for new user is normalized"""
+        email = 'test@EXAMPLE.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_validation(self):
+        """Test creating user with no email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_super_user_create(self):
+        """Test to create superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@titan.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
